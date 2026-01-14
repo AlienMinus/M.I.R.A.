0ea8dcb3aac1e4e5147fb0f36f97da3b6ea11bd5
AlienMinus <dasmanasranjan2005@gmail.com>
2026-01-14 17:39:51 +0530

    vite fix

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the API 🫱🏻‍🫲🏻' });
-});
-
-export default router;
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: "localhost",
+    port: 3000,
+    strictPort: true,
     hmr: {
-      host: 'localhost',
+      protocol: "ws",
+      host: "localhost",
+      port: 3000,
     },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    watch: {
+      usePolling: true,
     },
   },
-})
+});
