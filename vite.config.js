9d945db1eb5f0fbe7ebc847ab872ee9b0924f434
AlienMinus <dasmanasranjan2005@gmail.com>
2026-01-14 17:56:27 +0530

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "localhost",
-    port: 3000,
-    strictPort: true,
-    hmr: {
-      protocol: "ws",
-      host: "localhost",
-      port: 3000,
-    },
     watch: {
       usePolling: true,
+      interval: 300,
     },
   },
 });
